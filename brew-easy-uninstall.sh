af9652685038ebf6a18b3ff07c2aefc16028b40d
Modded Gamers <35778371+ModdedGamers@users.noreply.github.com>
2021-11-10 15:01:12 -0500

    Update brew-easy-uninstall.sh

--- brew-easy-uninstall.sh
+++ brew-easy-uninstall.sh
@@ -7,12 +7,12 @@
 # Set variables again
 export DIR="${HOME}/.brew"
 
-export RCFILES=(.bashrc .bash_profile .zshrc .profile .zprofile)
+export RCFILES=(.bashrc .zshrc)
 
 printf "Deleting variables\n\n"
 for file in "${RCFILES[@]}"; do sed -i asdf '/HOMEBREW/d' "$HOME/$file"; done
 for file in "${RCFILES[@]}"; do rm -rf "${file}asdf"; done
-for file in "${RCFILES[@]}"; do sed -i asdf '/\$HOME/.brew/d' "$HOME/$file"; done
+for file in "${RCFILES[@]}"; do sed -i asdf '/brew_init/d' "$HOME/$file"; done
 for file in "${RCFILES[@]}"; do rm -rf "${file}asdf"; done
 
 printf "Deleting ~/.brew\n\n"
